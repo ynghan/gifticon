43f52eea7286b49b12cbd900efefc505fd741165
민상기 <sgmin206@naver.com>
2025-03-14 15:22:41 +0900

    ✨[Feat] BottomBar UI 및 KakaoMap  : S12P21E106-101

--- FE/web/src/app/page.tsx
+++ FE/web/src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button';
+import { KakaoMap } from '@/features/map';
 
 export default function Home() {
   return (
     <>
-      <h1>홈페이지입니다</h1>
-      <Button variant={'outline'}>하이</Button>;
+      <KakaoMap />
     </>
   );
 }
