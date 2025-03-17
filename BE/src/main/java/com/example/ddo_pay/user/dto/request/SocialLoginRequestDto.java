6328a608ce693ed644c77769618c46e0e111b9b9
신주환 <yaya0147@gnu.ac.kr>
2025-03-17 17:56:49 +0900

    ✨ [Feat] User 도메인 API 작성 S12P21E106-131

---
+++ BE/src/main/java/com/example/ddo_pay/user/dto/request/SocialLoginRequestDto.java
@@ -0,0 +1,8 @@
+package com.example.ddo_pay.user.dto.request;
+
+import lombok.Data;
+
+@Data
+public class SocialLoginRequestDto {
+    private String socialId;
+}
---
+++ BE/src/main/java/com/example/ddo_pay/user/dto/response/SocialLoginResponseDto.java
@@ -0,0 +1,10 @@
+
+package com.example.ddo_pay.user.dto.response;
+
+import lombok.Data;
+
+@Data
+public class SocialLoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+}
