479f396fd7029f2f68375ffa6e64cdef7ba32a89
jeong young han <daum4572@naver.com>
2025-03-17 11:33:20 +0900

    :sparkles: [Feat] SpringBoot 초기 세팅 S12P21E106-107

---
+++ BE/src/main/java/com/example/ddo_pay/DdoPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddo_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdoPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdoPayApplication.class, args);
+	}
+
+}
