caa5b0a386946479e97df331179609c74307b652
이상혁 <lsh950523@naver.com>
2025-04-01 01:40:37 +0900

    Fix : S3Config Credentials 수정

--- BE/src/main/java/com/example/ddo_pay/common/config/S3/S3Config.java
+++ BE/src/main/java/com/example/ddo_pay/common/config/S3/S3Config.java
@@ -4,24 +4,40 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "cloud.aws")
+@Getter
+@Setter
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-    @Value("${cloud.aws.region.static}")
-    private String region;
+    private Credentials credentials;
+    private Region region;
+
+    @Getter @Setter
+    public static class Credentials {
+        private String accessKey;
+        private String secretKey;
+    }
+
+    @Getter @Setter
+    public static class Region {
+        private String static_;
+    }
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
+                credentials.accessKey,
+                credentials.secretKey
+        );
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+                .withRegion(region.static_)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
